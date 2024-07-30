7844cbb30410df321679407936e7712fc0f2b07b
Lucas Araujo <lucasaraujo4188@gmail.com>
2024-07-30 12:47:59 -0300

    add payment controller

---
+++ src/main/java/br/com/alurafood/payments/controller/PaymentController.java
@@ -0,0 +1,53 @@
+package br.com.alurafood.payments.controller;
+
+import br.com.alurafood.payments.dto.PaymentDTO;
+import br.com.alurafood.payments.service.PaymentService;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
+
+@RestController
+@RequestMapping("/api/payments")
+public class PaymentController {
+    @Autowired
+    private PaymentService service;
+
+    @GetMapping
+    public Page<PaymentDTO> findAll(@PageableDefault(size = 10) Pageable pagination) {
+        return service.findAll(pagination);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<PaymentDTO> findById(@PathVariable("id") Long id) {
+        PaymentDTO dto = service.findById(id);
+        return ResponseEntity.ok(dto);
+    }
+
+    @PostMapping
+    public ResponseEntity<PaymentDTO> create(@RequestBody @Valid PaymentDTO dto, UriComponentsBuilder uriBuilder)  {
+        PaymentDTO payment = service.create(dto);
+        URI address = uriBuilder.path("/api/payments/{id}").buildAndExpand(payment.getId()).toUri();
+
+        return ResponseEntity.created(address).body(payment);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<PaymentDTO> update(@PathVariable("id") @NotNull Long id, @RequestBody @Valid PaymentDTO dto) {
+        PaymentDTO updatedPayment = service.update(id, dto);
+        return ResponseEntity.ok(updatedPayment);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> delete(@PathVariable("id") @NotNull Long id) {
+        service.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+}
